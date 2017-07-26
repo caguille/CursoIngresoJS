f37e41ef973551d790615c8c668411f984f6aeea
caguille <cristianma26@gmail.com>
2017-07-25 21:43:02 -0300

    25-7

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,17 @@
 function Mostrar()
 {
+    
+    var numero= parseInt (prompt ("ingrese un numero y le dire los pares"));
+    var cont=0;
+    for (i=0;i<=numero;i++)
+        if (i%2==0)
+        {
+            cont++;
+            console.log (i);
+        }
 
 
 
 
 }//FIN DE LA FUNCIÓN
+
--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,5 +1,26 @@
 function Mostrar()
 {
+ var numero= parseInt (prompt ("ingrese un numero y le dire los pares"));
+    var cont=0;
+    for (i=1;i<=numero;i++)
+    {
+        if (numero%i==0)
+        {
+            cont++;
+            if (cont>2)
+            {
+                break;
+            }
+        }
+    }    
+    if(cont==2)
+    {
+        alert ("es numero primo")
+    }    
+    else
+    {
+            alert ("no es primo")
+    }
 
 
 
